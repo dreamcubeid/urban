b6a7d886f725f50279d123d25e582eb340208f52
MFauzi09 <m.fauzi09071998@gmail.com>
2021-10-04 20:45:18 +0700

    deduction

--- components/OrderSummaryBox/index.tsx
+++ components/OrderSummaryBox/index.tsx
@@ -32,6 +32,7 @@ const classesOrderSummary = {
   submitButtonClassName: styles.submitButton,
   expandButtonClassName: stylesPriceBreakdown.expandButton,
   expandedDivClassName: stylesPriceBreakdown.expandedDiv,
+  deductionPriceClassName: stylesPriceBreakdown.deductionPrice,
 
   /* applied coupon and point */
   voucherTextClassName: styles.voucherText,
